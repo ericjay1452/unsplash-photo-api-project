756f269c316b141ddc1f18e3ffd8e20422f6ca43
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-08 06:31:36 +0100

    added some imports to App comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-
+import { Photo } from "./components/Photo/Photo"
+import { FaSearch } from "react-icons/fa"
 
 function App() {
-return(
- 
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  
-)
+	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
