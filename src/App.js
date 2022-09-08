63c715a8b80c025fda428e9a127d94c64a38f79e
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-08 08:03:10 +0100

    started mapping my photo state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa"
-import { Photo } from "./components/Photo/Photo"
+import Photo from "./components/Photo/Photo"
 
 const mainUriSearch = `https://api.unsplash.com/photos/`;
 const searchUri = `https://api.unsplash.com/search/photos`;
@@ -18,19 +18,62 @@ function App() {
        
     try {
       const response = await fetch(Uri);
-      const data = await response.json()
-      console.log(data)
-    } catch (error) {
+     setPhotos(await response.json())
+     setLoading(false)
+      } 
+      catch (error) {
       setLoading(false)
       console.log(error)
     }
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSubmit = (e) => {
+   e.preventDefault()
+   console.log("Hello word")
   }
 
   useEffect ( () => {
     fetchUnsplashImage()
   }, [])
 
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+    <main className="block w-full relative bg-slate-300">
+
+      <section className="w-6/12 m-auto p-3 block relative h-full">
+       <form className="w-full relative">
+          <div className="relative block">
+           <label htmlFor="search"></label>
+           <input name="search"
+            id="search" 
+            placeholder="Search ..." 
+            value={""} 
+            onChange={handleChange}
+            className = "relative py-2 px-2 rounded-full border-0 text-center w-full hover:border-0 focus:border-0"/>
+
+              <button type="submit" aria-label="submit button" className="" onClick={handleSubmit}>
+                 <FaSearch className="absolute right-3 top-3 text-orange-400"/> 
+                 </button>
+                 </div>
+       </form>
+      </section>
+
+      <section>
+          <div>
+            {photos.map((photo) =>{
+              const {id} = photo
+              return (
+                <Photo key={id} photo={photo}/>
+              )
+            })}
+          </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
